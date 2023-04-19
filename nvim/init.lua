583c5cf3f7c93bc1ec6408afdcd81088b45b0d37
Amarpreet Singh <ampit.xd@gmail.com>
2023-04-19 22:27:53 +0530

    Remove dressing as it's not working

--- nvim/init.lua
+++ nvim/init.lua
@@ -270,24 +270,6 @@ require("lazy").setup({
 		end,
 	},
 
-	-- Dressing
-	{
-		"stevearc/dressing.nvim",
-		lazy = true,
-		init = function()
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.select = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.select(...)
-			end
-			---@diagnostic disable-next-line: duplicate-set-field
-			vim.ui.input = function(...)
-				require("lazy").load({ plugins = { "dressing.nvim" } })
-				return vim.ui.input(...)
-			end
-		end,
-	},
-
 	-- IndentScope
 	{ "echasnovski/mini.indentscope" },
 
