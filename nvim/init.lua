3a9a8b3380bc0957acaeeb3ba61bc323df310491
Amarpreet Singh <ampit.xd@gmail.com>
2023-04-30 02:21:55 +0530

    Show recording notification using noice

--- nvim/init.lua
+++ nvim/init.lua
@@ -263,7 +263,12 @@ require("lazy").setup({
 		"folke/noice.nvim",
 		config = function()
 			require("noice").setup({
-				-- add any options here
+				routes = {
+					{
+						view = "notify",
+						filter = { event = "msg_showmode" },
+					},
+				},
 			})
 		end,
 		dependencies = {
