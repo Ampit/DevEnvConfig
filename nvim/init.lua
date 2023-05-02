655c4c54489a7bbaa6f80f2a81f18503819a2831
Amarpreet Singh <ampit.xd@gmail.com>
2023-05-02 23:16:25 +0530

    tabby updates/ still not working

--- nvim/init.lua
+++ nvim/init.lua
@@ -958,54 +958,43 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 require("nvim-highlight-colors").turnOn()
 
 -- Tabs Setup
-local theme = {
-	fill = "TabLineFill",
-	-- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
-	head = "TabLine",
-	-- current_tab = 'TabLineSel',
-	current_tab = { fg = "#F8FBF6", bg = "#5E81AC", style = "italic" },
-	tab = "TabLine",
-	win = "TabLine",
-	tail = "TabLine",
+-- Define the custom Tokyo Night-based theme
+local tokyonight_tabby = {
+  fill = "TabLineFill",
+  head = { fg = "#c0caf5", bg = "#1a1b26", style = "italic" },
+  current_tab = { fg = "#ffffff", bg = "#7aa2f7", style = "italic" },
+  tab = { fg = "#c0caf5", bg = "#1a1b26", style = "italic" },
+  win = "TabLine",
+  tail = { fg = "#c0caf5", bg = "#1a1b26", style = "italic" },
 }
 
+-- Set the custom theme for Tabby
 require("tabby.tabline").set(function(line)
-	return {
-		{
-			{ "  ", hl = theme.head },
-			line.sep("", theme.head, theme.fill),
-		},
-		line.tabs().foreach(function(tab)
-			local hl = tab.is_current() and theme.current_tab or theme.tab
-			return {
-				line.sep("", hl, theme.fill),
-				tab.is_current() and "" or "",
-				tab.number(),
-				tab.name(),
-				-- tab.close_btn(''), -- show a close button
-				line.sep("", hl, theme.fill),
-				hl = hl,
-				margin = " ",
-			}
-		end),
-		line.spacer(),
-		-- shows list of windows in tab
-		-- line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-		--   return {
-		--     line.sep('', theme.win, theme.fill),
-		--     win.is_current() and '' or '',
-		--     win.buf_name(),
-		--     line.sep('', theme.win, theme.fill),
-		--     hl = theme.win,
-		--     margin = ' ',
-		--   }
-		-- end),
-		{
-			line.sep("", theme.tail, theme.fill),
-			{ "  ", hl = theme.tail },
-		},
-		hl = theme.fill,
-	}
+  return {
+    {
+      { "  ", hl = tokyonight_tabby.head },
+      line.sep("", tokyonight_tabby.head, tokyonight_tabby.fill),
+    },
+    line.tabs().foreach(function(tab)
+      local hl = tab.is_current() and tokyonight_tabby.current_tab or tokyonight_tabby.tab
+      return {
+        line.sep("", hl, tokyonight_tabby.fill),
+        tab.is_current() and "" or "",
+        tab.number(),
+        tab.name(),
+        tab.close_btn(''), -- show a close button
+        line.sep("", hl, tokyonight_tabby.fill),
+        hl = hl,
+        margin = " ",
+      }
+    end),
+    line.spacer(),
+    {
+      line.sep("", tokyonight_tabby.tail, tokyonight_tabby.fill),
+      { "  ", hl = tokyonight_tabby.tail },
+    },
+    hl = tokyonight_tabby.fill,
+  }
 end)
 
 -- Convert the Vimscript scrollbar autocmd block to Lua and execute it
@@ -1180,8 +1169,8 @@ for _, language in ipairs({ "typescript", "javascript" }) do
 			name = "Test:Server:Local:Module",
 			type = "pwa-node",
 			request = "launch",
-			program = "/opt/homebrew/Cellar/yarn/1.22.19/libexec/bin/yarn.js",
-			args = { "test:server:module:local"},
+			runtimeExecutable = "/opt/homebrew/Cellar/yarn/1.22.19/libexec/bin/yarn.js",
+			runtimeArgs = { "test:server:module:local"},
 			cwd = "${workspaceFolder}/packages/main-backend",
 		},
 		{
