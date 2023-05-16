c5d1619d2442b449f676fef899e0b998f5c9600d
Amarpreet Singh <ampit.xd@gmail.com>
2023-05-16 15:19:30 -0400

    remove extra comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,12 +27,9 @@ require("lazy").setup({
 		-- LSP Configuration & Plugins
 		"neovim/nvim-lspconfig",
 		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
-			-- Additional lua configuration, makes nvim stuff amazing!
 			"folke/neodev.nvim",
 			"jose-elias-alvarez/typescript.nvim",
 		},
