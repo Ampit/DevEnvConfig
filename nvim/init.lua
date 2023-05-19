499a29aa395e766fb4d35ded06754faa31cc53bd
Amarpreet Singh <ampit.xd@gmail.com>
2023-05-18 21:57:03 -0400

    remove extra comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -110,8 +110,6 @@ require("lazy").setup({
 	{
 		-- Add indentation guides even on blank lines
 		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help indent_blankline.txt`
 		opts = {
 			char = "┊",
 			show_trailing_blankline_indent = false,
@@ -127,13 +125,8 @@ require("lazy").setup({
 	},
 	-- Fuzzy Finder (files, lsp, etc)
 	{ "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } },
-	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
-	-- Only load if `make` is available. Make sure you have the system
-	-- requirements installed.
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
-		-- NOTE: If you are having trouble with this installation,
-		--       refer to the README for telescope-fzf-native for more instructions.
 		build = "make",
 		cond = function()
 			return vim.fn.executable("make") == 1
@@ -272,9 +265,6 @@ require("lazy").setup({
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
 	},
