2275ef8f1d6a2e7b981a69957e37892d3a6b9ff8
Amarpreet Singh <ampit.xd@gmail.com>
2024-06-22 12:17:39 -0400

    auto save, update deps

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,3 +12,23 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt.conceallevel = 0
   end,
 })
+
+-- Auto save
+function _G.format_save_current_buffer()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.buf_get_clients(bufnr)
+
+  for _, client in ipairs(clients) do
+    if client ~= nil and client.resolved_capabilities and client.resolved_capabilities.document_formatting then
+      vim.cmd("silent! Format")
+      break
+    end
+  end
+
+  -- Check if the buffer is modifiable before saving
+  if vim.api.nvim_buf_get_option(bufnr, "modifiable") then
+    vim.cmd("silent! wa")
+  end
+end
+
+vim.cmd("au BufLeave,FocusLost * lua format_save_current_buffer()")
