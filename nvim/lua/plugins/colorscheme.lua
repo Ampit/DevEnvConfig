0424c3c6a7c971582427e22ad36c955e57248c7d
Amarpreet Singh <ampit.xd@gmail.com>
2024-08-14 14:06:56 -0400

    update deps, fix transparency issue in tmux

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,20 @@
 return {
   "folke/tokyonight.nvim",
-  lazy = true,
+  lazy = false,
   opts = {
     on_highlights = function(hl, c)
-      hl.IndentBlanklineContextChar = {
-        fg = c.dark5,
-      }
-      hl.TSConstructor = {
-        fg = c.blue1,
-      }
-      hl.TSTagDelimiter = {
-        fg = c.dark5,
-      }
+      -- hl.Normal = { bg = "NONE" }
+      -- hl.NormalNC = { bg = "NONE" } -- Non-current window
+      hl.NvimTreeNormal = { bg = "NONE" } -- Neotree/NvimTree background
+      hl.NvimTreeNormalNC = { bg = "NONE" } -- Neotree/NvimTree non-current window
+      -- hl.FloatBorder = { bg = "NONE" } -- Floating window borders
+      -- hl.NormalFloat = { bg = "NONE" } -- Floating windows background
+      hl.IndentBlanklineContextChar = { fg = c.dark5 }
+      hl.TSConstructor = { fg = c.blue1 }
+      hl.TSTagDelimiter = { fg = c.dark5 }
     end,
     style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-    transparent = false,
+    transparent = true,
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles = {
       -- Style to be applied to different syntax groups
@@ -24,7 +24,7 @@ return {
       functions = {},
       variables = {},
       -- Background styles. Can be "dark", "transparent" or "normal"
-      sidebars = "dark", -- style for sidebars, see below
+      sidebars = "transparent", -- style for sidebars, see below
       floats = "dark", -- style for floating windows
     },
     -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
@@ -36,6 +36,7 @@ return {
       "spectre_panel",
       "NeogitStatus",
       "help",
+      "NeoTree",
     },
     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
