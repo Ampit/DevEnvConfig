cdcdf8afc98383a9e3db47430ceaaac03fe76065
Amarpreet Singh <ampit.xd@gmail.com>
2024-10-17 22:57:38 -0400

    format, remove diffview keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
 -- local discipline = require("config.discipline")
 --
 -- discipline.cowboy()
-
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
+local opts = {
+    noremap = true,
+    silent = true
+}
 
 keymap.set("n", "x", '"_x')
 
@@ -22,7 +23,7 @@ keymap.set("n", "dw", 'vb"_d')
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Save with root permission (not working for now)
---vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+-- vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- Disable continuations
 keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
@@ -39,28 +40,61 @@ keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 -- Diagnostic keymaps
-keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-keymap.set("n", "<leader>fd", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+keymap.set("n", "[d", vim.diagnostic.goto_prev, {
+    desc = "Go to previous diagnostic message"
+})
+keymap.set("n", "]d", vim.diagnostic.goto_next, {
+    desc = "Go to next diagnostic message"
+})
+keymap.set("n", "<leader>fd", vim.diagnostic.open_float, {
+    desc = "Open floating diagnostic message"
+})
+keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, {
+    desc = "Open diagnostics list"
+})
 
 -- DiffviewOpen
-keymap.set("n", "<leader>dvo", ":DiffviewOpen<CR>", { desc = "Open Diffview" })
-keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>", { desc = "Close Diffview" })
-
+keymap.set("n", "<leader>dvo", ":DiffviewOpen<CR>", {
+    desc = "Open Diffview"
+})
+keymap.set("n", "<leader>dvc", ":DiffviewClose<CR>", {
+    desc = "Close Diffview"
+})
 -- Undo Tree
-keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undo Tree" })
+keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {
+    desc = "Undo Tree"
+})
 
 -- Additional key mappings for multiple terminals
-keymap.set("n", "<leader>2", "<cmd>2ToggleTerm<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>3", "<cmd>3ToggleTerm<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>ts", "<cmd>TermSelect<CR>", { noremap = true, silent = true })
-keymap.set("n", "<leader>ta", "<cmd>ToggleTermToggleAll<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>2", "<cmd>2ToggleTerm<CR>", {
+    noremap = true,
+    silent = true
+})
+keymap.set("n", "<leader>3", "<cmd>3ToggleTerm<CR>", {
+    noremap = true,
+    silent = true
+})
+keymap.set("n", "<leader>ts", "<cmd>TermSelect<CR>", {
+    noremap = true,
+    silent = true
+})
+keymap.set("n", "<leader>ta", "<cmd>ToggleTermToggleAll<CR>", {
+    noremap = true,
+    silent = true
+})
 
 -- Swift-specific keymaps
-keymap.set("n", "<leader>sr", ":!swift run<CR>", { buffer = true, desc = "Swift Run" })
-keymap.set("n", "<leader>sb", ":!swift build<CR>", { buffer = true, desc = "Swift Build" })
+keymap.set("n", "<leader>sr", ":!swift run<CR>", {
+    buffer = true,
+    desc = "Swift Run"
+})
+keymap.set("n", "<leader>sb", ":!swift build<CR>", {
+    buffer = true,
+    desc = "Swift Build"
+})
 
 -- Set Ctrl n to forward jump list
-keymap.set("n", "<C-n>", "<C-i>", { noremap = true, silent = true })
-
+keymap.set("n", "<C-n>", "<C-i>", {
+    noremap = true,
+    silent = true
+})
--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,166 +1,184 @@
 return {
-  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
-  {
-    "danymat/neogen",
-    keys = {
-      {
-        "<leader>cc",
-        function()
-          require("neogen").generate({})
-        end,
-        desc = "Neogen Comment",
-      },
-    },
-    opts = { snippet_engine = "luasnip" },
-  },
-
-  -- Incremental rename
-  {
-    "smjonas/inc-rename.nvim",
-    cmd = "IncRename",
-    config = true,
-  },
-
-  -- Refactoring tool
-  {
-    "ThePrimeagen/refactoring.nvim",
-    keys = {
-      {
-        "<leader>r",
-        function()
-          require("refactoring").select_refactor()
-        end,
-        mode = "v",
-        noremap = true,
-        silent = true,
-        expr = false,
-      },
-    },
-    opts = {},
-  },
-
-  -- Go forward/backward with square brackets
-  {
-    "echasnovski/mini.bracketed",
-    event = "BufReadPost",
-    config = function()
-      local bracketed = require("mini.bracketed")
-      bracketed.setup({
-        file = { suffix = "" },
-        window = { suffix = "" },
-        quickfix = { suffix = "" },
-        yank = { suffix = "" },
-        treesitter = { suffix = "n" },
-      })
-    end,
-  },
-
-  -- Better increase/descrease
-  {
-    "monaqa/dial.nvim",
+	{
+		-- Create annotations with one keybind, and jump your cursor in the inserted annotation
+		"danymat/neogen",
+		keys = {
+			{
+				"<leader>cc",
+				function()
+					require("neogen").generate({})
+				end,
+				desc = "Neogen Comment",
+			},
+		},
+		opts = {
+			snippet_engine = "luasnip",
+		},
+	},
+	{
+		-- Incremental rename
+		"smjonas/inc-rename.nvim",
+		cmd = "IncRename",
+		config = true,
+	},
+	{
+		-- Refactoring tool
+		"ThePrimeagen/refactoring.nvim",
+		keys = {
+			{
+				"<leader>r",
+				function()
+					require("refactoring").select_refactor()
+				end,
+				mode = "v",
+				noremap = true,
+				silent = true,
+				expr = false,
+			},
+		},
+		opts = {},
+	},
+	{
+		-- Go forward/backward with square brackets
+		"echasnovski/mini.bracketed",
+		event = "BufReadPost",
+		config = function()
+			local bracketed = require("mini.bracketed")
+			bracketed.setup({
+				file = {
+					suffix = "",
+				},
+				window = {
+					suffix = "",
+				},
+				quickfix = {
+					suffix = "",
+				},
+				yank = {
+					suffix = "",
+				},
+				treesitter = {
+					suffix = "n",
+				},
+			})
+		end,
+	},
+	{
+		-- Better increase/descrease
+		"monaqa/dial.nvim",
     -- stylua: ignore
-    keys = {
-      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-    },
-    config = function()
-      local augend = require("dial.augend")
-      require("dial.config").augends:register_group({
-        default = {
-          augend.integer.alias.decimal,
-          augend.integer.alias.hex,
-          augend.date.alias["%Y/%m/%d"],
-          augend.constant.alias.bool,
-          augend.semver.alias.semver,
-          augend.constant.new({ elements = { "let", "const" } }),
-        },
-      })
-    end,
-  },
-
-  {
-    "simrat39/symbols-outline.nvim",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    cmd = "SymbolsOutline",
-    opts = {
-      position = "right",
-    },
-  },
-
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
-  {
-    -- Wakatime
-    "wakatime/vim-wakatime",
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = true },
-      panel = { enabled = true },
-      filetypes = {
-        markdown = true,
-        help = true,
-        yaml = true,
-      },
-    },
-  },
-  -- Git Blame
-  "f-person/git-blame.nvim",
-  -- Diff View
-  {
-    "sindrets/diffview.nvim",
-    opts = {
-      keymaps = {
-        view = {
-          ["<tab>"] = false, -- Disable the default tab keymap
-          ["<S-tab>"] = "select_next_entry", -- Use <S-tab> for previous entry
-          ["<C-S-tab>"] = "select_prev_entry", -- Use <C-S-tab> for next entry
-        },
-      },
-    },
-  },
-  -- Smooth Scroll
-  "karb94/neoscroll.nvim",
-  -- Scrollbar
-  "Xuyuanp/scrollbar.nvim",
-  -- Telescope file browser
-  -- Undotree
-  { "mbbill/undotree", config = function() end },
-  -- Peepsight
-  "koenverburg/peepsight.nvim",
-  -- Highlight words
-  "Mr-LLLLL/interestingwords.nvim",
-  -- Pretty Fold
-  -- { "anuvyklack/pretty-fold.nvim", opts = {} },
-  -- Fold Preview
-  {
-    "anuvyklack/fold-preview.nvim",
-    dependencies = { "anuvyklack/keymap-amend.nvim" },
-    opts = {},
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+    keys = {{
+        "<C-a>",
+        function()
+            return require("dial.map").inc_normal()
+        end,
+        expr = true,
+        desc = "Increment"
+    }, {
+        "<C-x>",
+        function()
+            return require("dial.map").dec_normal()
+        end,
+        expr = true,
+        desc = "Decrement"
+    }},
+		config = function()
+			local augend = require("dial.augend")
+			require("dial.config").augends:register_group({
+				default = {
+					augend.integer.alias.decimal,
+					augend.integer.alias.hex,
+					augend.date.alias["%Y/%m/%d"],
+					augend.constant.alias.bool,
+					augend.semver.alias.semver,
+					augend.constant.new({
+						elements = { "let", "const" },
+					}),
+				},
+			})
+		end,
+	},
+	{
+		"simrat39/symbols-outline.nvim",
+		keys = { {
+			"<leader>cs",
+			"<cmd>SymbolsOutline<cr>",
+			desc = "Symbols Outline",
+		} },
+		cmd = "SymbolsOutline",
+		opts = {
+			position = "right",
+		},
+	},
+	{
+		"nvim-cmp",
+		dependencies = { "hrsh7th/cmp-emoji" },
+		opts = function(_, opts)
+			table.insert(opts.sources, {
+				name = "emoji",
+			})
+		end,
+	},
+	{ -- Wakatime
+		"wakatime/vim-wakatime",
+	},
+	{
+		-- Copilot
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		opts = {
+			suggestion = {
+				enabled = true,
+			},
+			panel = {
+				enabled = true,
+			},
+			filetypes = {
+				markdown = true,
+				help = true,
+				yaml = true,
+			},
+		},
+	},
+	{ -- Git Blame
+		"f-person/git-blame.nvim",
+	},
+	{ -- Diff View
+		"sindrets/diffview.nvim",
+	},
+	{ -- Smooth Scroll
+		"karb94/neoscroll.nvim",
+	},
+	{ -- Scrollbar
+		"Xuyuanp/scrollbar.nvim",
+	},
+	{
+		-- Undotree
+		"mbbill/undotree",
+		config = function() end,
+	}, -- Peepsight
+	{ -- Highlight words
+		"koenverburg/peepsight.nvim",
+	},
+	{ -- Pretty Fold
+		"Mr-LLLLL/interestingwords.nvim",
+	},
+	{
+		-- Fold Preview
+		"anuvyklack/fold-preview.nvim",
+		dependencies = { "anuvyklack/keymap-amend.nvim" },
+		opts = {},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = { "TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateUp", "TmuxNavigateRight", "TmuxNavigatePrevious" },
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 }
