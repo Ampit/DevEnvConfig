f339a71338d297f752c9725222334ad095c1d705
Amarpreet Singh <ampit.xd@gmail.com>
2024-11-20 15:15:19 -0500

    fix error

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -138,7 +138,9 @@ return {
       ]]
 
       logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
+      opts.config = {
+        header = vim.split(logo, "\n"),
+      }
     end,
   },
 }
